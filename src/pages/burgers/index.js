aa8e234b989516dff9a6efd4e6519a6f5cf8c059
Eugene Pavlovskiy <infopavlovskiy@gmail.com>
2023-01-23 23:09:28 +0100

    index-js map

--- src/pages/burgers/index.js
+++ src/pages/burgers/index.js
@@ -4,6 +4,7 @@ import styles from '../../styles/Burgers.module.css';
 
 
 export const getStaticProps = async () => {
+
   const res = await fetch('http://localhost:5000/items');
   const data = await res.json();
 
@@ -14,33 +15,27 @@ export const getStaticProps = async () => {
 
 
 const Burgers = ({ burgers}) => {
-
   return (
     <div>
       <h1>Наши Бургеры</h1>
-      {burgers.map(burger => {
-        return (
-          <Link href={``} key={burger.id}>
-          <a className={styles.burgerCard}>
+      {burgers.map(burger => (
+          <Link href={`/burgers/` + burger.id} key={burger.id} className={styles.burgerCard}>
             <div className={styles.imageContainer}>
               <Image 
                 src={`${burger.image}`} 
                 alt={`${burger.name}`} 
-                width="100%" 
-                height="100%" 
-                layout="responsive" 
-                objectFit="cover"
+                width={200} 
+                height={200} 
+                responsive
+                cover
               />
             </div>
             <div>
               <h3>{ burger.name }</h3>
               <p>{ burger.desc }</p>
             </div>
-          </a>
         </Link>
-        )
-        console.log(burger)
-      })}
+        ))}
     </div>
   );
 }
